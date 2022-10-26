f3032b0ef3f538b492737dbc38f1b54fce69ef91
hwangjinseong <imhjs2023@gmail.com>
2022-10-26 21:52:00 +0900

    ⚡️ :: 이미지 추가

--- src/assets/Img/index.js
+++ src/assets/Img/index.js
@@ -1,7 +1,7 @@
-export { default as Alram } from "../Img/alram";
-export { default as ContentsBtn } from "../Img/contentsBtn";
-export { default as Logo } from "../Img/logo";
-export { default as Refresh } from "../Img/refresh";
-export { default as Search } from "../Img/search";
-export { default as User } from "../Img/user";
-export { default as Good } from "../Img/good";
+export { default as Alram } from "./alram.svg";
+export { default as ContentsBtn } from "./contentsBtn.svg";
+export { default as HeadLogo } from "./headlogo.svg";
+export { default as Refresh } from "./refresh.svg";
+export { default as Search } from "./search.svg";
+export { default as User } from "./user.svg";
+export { default as Good } from "./good.svg";
