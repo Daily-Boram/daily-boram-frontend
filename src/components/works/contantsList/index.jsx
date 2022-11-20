74f065f5db035bf84d7d54444acaf5afa3149fa3
hwangjinseong <imhjs2023@gmail.com>
2022-11-21 08:49:57 +0900

    ⚡️ :: 워크페이지 수정

--- src/components/works/contantsList/index.jsx
+++ src/components/works/contantsList/index.jsx
@@ -1,21 +1,23 @@
 import styled from "styled-components";
-import { SpacePhoto3 } from "../../../assets/Img";
+import { SpacePhoto3, BigGood } from "../../../assets/Img";
 
-const ContantsList = ({ number, title, date, price }) => {
+const ContantsList = ({ number, title, date, likenumber, price }) => {
   return (
-    <>
-      <ContantsListContainer>
-        <Photo src={SpacePhoto3} alt="우주 사진" />
-        <Information>
-          <div>
-            <Number>{number}</Number>
-            <Title>{title}</Title>
-            <Date>{date}</Date>
-          </div>
-          <Price>{price}</Price>
-        </Information>
-      </ContantsListContainer>
-    </>
+    <ContantsListContainer>
+      <Photo src={SpacePhoto3} alt="우주 사진" />
+      <Information>
+        <Left>
+          <Number>{number}</Number>
+          <Title>{title}</Title>
+          <Date>{date}</Date>
+          <Like>
+            <Icon src={BigGood} />
+            <LikeNumber>{likenumber}</LikeNumber>
+          </Like>
+        </Left>
+        <Price>{price}</Price>
+      </Information>
+    </ContantsListContainer>
   );
 };
 
@@ -43,6 +45,13 @@ const Information = styled.div`
   margin-left: 30px;
 `;
 
+const Left = styled.div`
+  height: 130px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
 const Number = styled.p`
   font-size: 25px;
   font-weight: bold;
@@ -52,12 +61,28 @@ const Title = styled.p`
   font-size: 25px;
   font-weight: bold;
   color: ${({ theme }) => theme.color.gray02};
-  margin-bottom: 30px;
 `;
 
 const Date = styled.p`
   font-size: 16px;
   color: ${({ theme }) => theme.color.gray02};
+`;
+
+const Like = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Icon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-right: 10px;
+`;
+
+const LikeNumber = styled.p`
+  color: ${({ theme }) => theme.color.gray02};
+  font-weight: 500;
+  font-size: 16px;
 `;
 
 const Price = styled.p`
