e4d56aac9e48db14f6091c01babd16473d44df06
kimkh05 <nestjs01@naver.com>
2022-12-06 16:17:04 +0900

    fix: 더미이미지 값이 없을때 기본 이미지 띄우기

--- src/components/common/contents/index.jsx
+++ src/components/common/contents/index.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const ContentsList = ({ number, title, date, likenumber, price, image }) => {
   return (
-    <ContentsListContainer to="/seeNovel">
-      <img src={SpacePhoto3} alt="image" />
+    <ContentsListContainer to={`/seeNovel/${number}`}>
+      <img src={image ? image : SpacePhoto3} alt="image" />
       <Information>
         <Left>
           <Number>{number}</Number>
